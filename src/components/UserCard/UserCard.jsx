df0b9583dc1c617f5d60b96ee5b1455d2cd57623
goharstepanian <gohar.stepanyan.05@mail.ru>
2025-05-09 22:46:27 +0400

    added memo to UserCard component

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,19 +1,25 @@
-import React from "react";
-import styles from "./UserCard.module.css"
+import React, { memo } from "react";
+import styles from "./UserCard.module.css";
 import userImg from "../../assets/user.webp";
 import { NavLink } from "react-router-dom";
-const UserCard = ({ user }) => {
- 
 
-  return (
-    <div className={styles.user}>
-      <NavLink to={`/profile/${user?.id}`}>
-        <img src={user?.photos?.large === null ? userImg : user?.photos?.large} className={styles.userImg } />
-      </NavLink>
+const UserCard = memo(
+  ({ user }) => {
+    return (
+      <div className={styles.user}>
+        <NavLink to={`/profile/${user?.id}`}>
+          <img
+            src={user?.photos?.large === null ? userImg : user?.photos?.large}
+            className={styles.userImg}
+            alt="User"
+          />
+        </NavLink>
         <h2>{user?.name}</h2>
-
-    </div>
-  );
-};
+      </div>
+    );
+  },
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps) === JSON.stringify(nextProps)
+);
 
 export default UserCard;
